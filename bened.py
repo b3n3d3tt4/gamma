30ae947525c347aa19b5b7bcb090e679d4339860
b3n3d3tt4 <raserab3nedetta@gmail.com>
2024-12-02 19:29:57 +0100

    salvo plot

--- bened.py
+++ bened.py
@@ -415,6 +415,7 @@ def linear_regression(x, y, sx=None, sy=None, xlabel="X-axis", ylabel="Y-axis", 
     plt.title(titolo)
     plt.grid(alpha=0.5)
     plt.legend()
+    plt.savefig(f'{titolo}.pdf')
     plt.show()
 
     # Plot dei residui
@@ -432,6 +433,7 @@ def linear_regression(x, y, sx=None, sy=None, xlabel="X-axis", ylabel="Y-axis", 
     plt.title("Residuals")
     plt.grid(alpha=0.5)
     plt.legend()
+    plt.savefig(f'residui_{titolo}.pdf')
     plt.show()
 
     return m, q, m_uncertainty, q_uncertainty, residui, chi_squared, chi_squared_reduced
